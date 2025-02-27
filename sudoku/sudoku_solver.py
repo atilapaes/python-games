5cadb9a9ae741190a1c97fae39775554b199b3c3
Atila da Silva Paes <atila.paes@gmail.com>
2025-02-26 21:05:44 -0700

    leetcode-sql50: Add exercise #8

--- sudoku/sudoku_solver.py
+++ sudoku/sudoku_solver.py
@@ -8,7 +8,7 @@ raw = genfromtxt('sample2.csv', delimiter=',',dtype=int)
 solution=raw
 
 
-def block_limits(block_number):
+def block_limits(block):
     """
     The 9x9 matrix is divided in 9 3x3 matrixes, that are named acording to the schema
     
@@ -38,7 +38,7 @@ def block_limits(block_number):
     [[6,3],[8,5]],
     [[6,6],[8,8]]])
     
-    return(blocks[block_number])
+    return(blocks[block])
 
 
 def create_workspace():
@@ -111,9 +111,9 @@ def initial_conditions(ws,sol):
             
     return(ws)
 
+
 def loop_rows(ws,sol):
     # loop for the rows
-    print('Loop over rows')
     for i in range(9):
         keys = shrink_vec(np.unique(sol[i,:])) # there are the values I need to remove from the first row
         for j in range(9):
@@ -129,9 +129,8 @@ def loop_rows(ws,sol):
 
 def loop_columns(ws,sol):
     #loop for the columns
-    print('Loop over column')
     for j in range(9):
-        keys = shrink_vec(np.unique(raw[:,j])) # there are the values I need to remove from the first row
+        keys = shrink_vec(np.unique(sol[:,j])) # there are the values I need to remove from the first row
         for i in range(9):
             if sol[i,j] == 0:
                 for key in keys:
@@ -139,39 +138,98 @@ def loop_columns(ws,sol):
                         ws[i,j]=remove_value_from_vec(ws[i,j],key)    
     
     sol=update_sol(ws,sol)
-    
     return([ws,sol])
 
 
+def loop_blocks(ws,sol):
+
+    for block in range(9):
+        block_keys=shrink_vec(sol[block_limits(block)[0][0]:block_limits(block)[1][0]+1,block_limits(block)[0][1]:block_limits(block)[1][1]+1])
+        
+        for i in range(block_limits(block)[0][0],block_limits(block)[1][0]+1):
+            for j in range(block_limits(block)[0][1],block_limits(block)[1][1]+1):
+                if sol[i,j] ==0:
+                    for key in block_keys:
+                        if key in ws[i,j] and len(shrink_vec(ws[i,j])) >1:
+                            ws[i,j]=remove_value_from_vec(ws[i,j],key)
+                            if len(shrink_vec(ws[i,j]))==1:
+                                print('new key found in',i,j,',block',block,', key',key)
+        
+    sol=update_sol(ws,sol)
+    return([ws,sol])
 
 #=======    
 # Aplly firt checking, with all the rows ans columns
 
 #step zero: create the matrix of possible keys
 workspace=create_workspace()
-print('step 0 done. N zeros:',np.count_nonzero(workspace==0))
+print('Step 0 done. N zeros:',np.count_nonzero(workspace==0))
 
 #step 1: apply the initial condition of the sudoku
 workspace=initial_conditions(workspace,solution)
-print('step 1 done. N zeros:',np.count_nonzero(workspace==0))
+print('Step 1. initial_conditions. N zeros:',np.count_nonzero(workspace==0))
+# %%
+
+
+ws=workspace
+sol=solution
+
+# loop for the rows
+for i in range(3):
+    keys = shrink_vec(np.unique(sol[i,:])) # there are the values I need to remove from the first row
+    for j in range(3):
+        if sol[i,j] == 0:
+            for key in keys:
+                if num_options(ws[i,j]) >1 and key in ws[i,j]: #otherwise, it will remove the key from its native place
+                    #print('changing in', i,j,'.key',key)    
+                    #print('ws before')
+                    #print(ws[i,j])
+                    ws[i,j]=remove_value_from_vec(ws[i,j],key)
+                    #print('ws after')
+                    #print(ws[i,j])
+sol=update_sol(ws,sol)
+
+
+for j in range(9):
+    keys = shrink_vec(np.unique(sol[:,j])) # there are the values I need to remove from the first row
+    for i in range(9):
+        if sol[i,j] == 0:
+            for key in keys:
+                if num_options(ws[i,j]) >1 and key in ws[i,j]: #otherwise, it will remove the key from its native place
+                    print('changing in', i,j,'.key',key)    
+                    #print('ws before')
+                    #print(ws[i,j])
+                    ws[i,j]=remove_value_from_vec(ws[i,j],key)
+                    print('ws after')
+                    print(ws[i,j])    
+
+sol=update_sol(ws,sol)
+
+
+
+
+# %%
+
+
 
 #step 2: loop over rows, and remove possible values for the keys present in the row
-[workspace,solution]=loop_rows(workspace,solution)
-print('step 2 done. N zeros:',np.count_nonzero(workspace==0))
+#[workspace,solution]=loop_rows(workspace,solution)
+#print('Step 2. Loop over rows. N zeros:',np.count_nonzero(workspace==0))
 
 #step 3: loop over columns, and remove possible values for the keys present in the columns
-[workspace,solution]=loop_columns(workspace,solution)
-print('step 3 done. N zeros:',np.count_nonzero(workspace==0))
+#[workspace,solution]=loop_columns(workspace,solution)
+#print('Step 3. Loop over columns. N zeros:',np.count_nonzero(workspace==0))
+
+
+#[workspace,solution]=loop_blocks(workspace,solution)
+#print('step 4. Loop over blocks. N zeros:',np.count_nonzero(workspace==0))
 
 #loop_over_blocks(workspace,raw)
 
 # %%
 
-
-
-# %%
-
-            
-
-
+#for i in range(9):
+#    for j in range(9):
+#        if raw[i,j]==0 and solution[i,j] !=0:
+#            print('new key in ',i,j, 'as ', solution[i,j])
 
